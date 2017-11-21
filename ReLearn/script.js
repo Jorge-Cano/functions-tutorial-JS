363d805d79d8205621d4a9841e00d6b459229889
Jorge A. Cano <jacinaustin@gmail.com>
2017-11-20 22:18:27 -0600

    Add files via upload
    recap on functions and inspector tools to reflect what is happening for a beginner

---
+++ ReLearn/script.js
@@ -0,0 +1,12 @@
+// Function: Declarations
+function plus(a,b) {
+  var sum = a+b;
+  return sum;
+};
+// console.log(plus(2,6));
+
+
+// Function: Definition Expressions
+// var plus = function(a,b) {
+//   return console.log(a+b);
+// }(4,8);
